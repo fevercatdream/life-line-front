09a225b691e72024a8e6925e0489514aa5ece33c
Tanya Silyutina <48433669+TanyaSilyutina@users.noreply.github.com>
2023-06-07 13:37:01 -0700

    friends refactor

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Timeline from './pages/timeline/Timeline.js';
 import Profile from './pages/profile/Profile.js';
 import Friends from './pages/friends/Friends.js';
 import Signup from './pages/signup/Signup.js';
-import Users from './pages/users/Users.js';
-import Ignored from './pages/ignored/Ignored.js';
 import EditProfile from './pages/profileEdit/profileEdit.js';
 import EditEvent from './pages/editEvent/editEvent.js';
 import NewEvent from './pages/newEvent/newEvent.js';
@@ -20,9 +18,7 @@ function App() {
         <Route path="/" element={<Homepage/>}/>
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/profile" element={<Profile/>}/>
-        <Route path="/friends" element={<Friends/>}/>
-        <Route path="/users" element={<Users/>}/>
-        <Route path="/ignored" element={<Ignored/>}/>
+        <Route path="/friends/*" element={<Friends/>}/>
         <Route path="/timeline/:id?" element={<Timeline/>}/>
         <Route path="/editprofile" element={<EditProfile/>}/>
         <Route path="/editevent" element={<EditEvent/>}/>
--- src/components/Friends/index.js
+++ src/components/Friends/index.js
@@ -1,65 +1,11 @@
-import React, {useEffect, useState} from 'react';
-import PersonRemove from '@mui/icons-material/PersonRemove';
-import Block from '@mui/icons-material/Block';
-import PersonAddAlt1 from "@mui/icons-material/PersonAddAlt1";
-import Clear from "@mui/icons-material/Clear";
-import {sendJSONRequest} from "../../utils/helpers";
-import {PendingOutlined, Person} from "@mui/icons-material";
-import {Link} from "react-router-dom";
+import FriendCard from './card';
+import MyFriends from './friends';
+import AllUsers from './users';
+import IgnoredUsers from './ignored';
 
-export function FriendCard({user, context}) {
-    const [pendingFriend, setPendingFriend] = useState();
-    const [isFriend, setisFriend] = useState();
-
-    useEffect(() => {
-        if (!user) {
-            return;
-        }
-        setPendingFriend(user.isPending)
-        setisFriend(user.isFriend);
-    }, [user])
-
-    const addFriend = async () => {
-        const res = await sendJSONRequest('POST', '/api/friends/create-request', {
-            friendId: user.id,
-        }, true)
-        if (res.status !== 200) {
-            return;
-        }
-        setPendingFriend(true);
-    }
-
-    let showBlockButton = true;
-    let action;
-    if (context === 'friend') {
-        action = <button className='deleteFriend'><PersonRemove sx={{ fontSize: 40 }}/></button>;
-    } else if (context === 'user') {
-        if (isFriend) {
-            action = <Person sx={{fontSize: 40}} color={'success'} />
-        } else if (pendingFriend) {
-            action = <PendingOutlined sx={{fontSize: 40}} />
-        } else {
-            action = <button className='addFriend'><PersonAddAlt1 sx={{fontSize: 40}} onClick={addFriend}/></button>;
-        }
-    } else if (context === 'ignored') {
-        action = <button className='deleteFriend'><Clear sx={{ fontSize: 40 }}/></button>;
-        showBlockButton = false;
-    }
-    return (
-        <figure className='friendCard2'>
-            {showBlockButton && <Block sx={{ fontSize: 35 }} className='blockUser'/> }
-            <div className='friendPhotoContainer'>
-                <Link to={`/timeline/${user.id}`}>
-                    <img className="friendPhoto" src={user.profilePhoto} alt={'friend profile'} />
-                </Link>
-            </div>
-            <figcaption className='friendBio'>
-                <div className='friendBioName'>
-                    <p className='smallerP'>{user.name}</p>
-                    <p className='smallerP'>{user.location}</p>
-                </div>
-                {action}
-            </figcaption>
-        </figure>
-    )
+export {
+    FriendCard,
+    MyFriends,
+    AllUsers,
+    IgnoredUsers,
 }
